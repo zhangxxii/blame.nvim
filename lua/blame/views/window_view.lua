8e57001e74d4107081465c9632e183ffc1c22efa
zhangzhijun.zzz <zhangzhijun.zzz@bytedance.com>
2024-07-11 21:40:51 +0800

    del loog

--- lua/blame/views/window_view.lua
+++ lua/blame/views/window_view.lua
@@ -410,7 +410,6 @@ function WindowView:setup_autocmd()
 
     vim.api.nvim_create_autocmd({ "CursorMoved" }, {
         callback = function()
-            print("CursorMoved...." .. os.time())
             if self.commit_info:is_open() == true then
                 self.commit_info:close(false)
             end
